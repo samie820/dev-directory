6efce808e29801da6178e2881ef4544335c3fcf0
samie820 <samuelomole70@gmail.com>
2020-01-21 23:20:33 +0700

    Setup tailwind css for gridsome

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,8 +3,19 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwindcss = require("tailwindcss")
+
 
 module.exports = {
   siteName: 'WeCode',
-  plugins: []
+  plugins: [],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          tailwindcss
+        ],
+      },
+    },
+  }
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,6 @@
   <Layout>
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
 
     <h1>Hello, world!</h1>
 
