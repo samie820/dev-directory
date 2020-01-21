027e5c1b2df6388a10899449b803e3cb094833ee
samie820 <samuelomole70@gmail.com>
2020-01-22 00:09:01 +0700

    Create the footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="flex px-16 justify-center border-t-2 bg-black h-32">
+    <div class="flex justify-between w-full items-center bg-transparent">
+      <g-link class="text-white">THE TECH DIRECTORY</g-link>
+      <g-link class="text-white">About</g-link>
+      <g-link class="text-white">Contact</g-link>
+      <g-link class="text-white">©TTD. 2020</g-link>
+    </div>
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: 'Footer'
+    }
+</script>
