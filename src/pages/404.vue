6be0c00f3be72f15a17728759d62e6e8c54e86bf
Samuel Omole <samuelomole70@gmail.com>
2020-01-29 14:45:05 +0700

    Create the not found page

---
+++ src/pages/404.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full h-screen flex justify-center items-center">
+        <g-image alt="not found illustration" src="~/assets/images/notFound.svg" />
+    </div>
+</template>
