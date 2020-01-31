b2332d4389c3716a0b4ade079f673cdb8e713dd3
Samuel Omole <samuelomole70@gmail.com>
2020-01-31 10:49:59 +0700

    slight refactor of the user card

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -2,7 +2,7 @@
     <div class="p-8 relative">
         <div class="card-header flex flex-row items-center relative">
             <div class="relative">
-                <img src="" alt="" class="avatar">
+                <g-image :src="userProfile" alt="" class="avatar" />
                 <span class="h-6 w-6 absolute verified">
                     <verified-badge v-if="user.isVerified"></verified-badge>
                 </span>
@@ -21,14 +21,14 @@
              <p class="text-gray-500 text-base mt-3 mb-6">{{user.bio}}</p>
         </div>
         <div class="card-footer absolute w-1/3 bg-black bottom-0 right-0 rounded-br flex">
-            <a href="#" class="text-white capitalize text-sm font-light flex-grow text-center py-4">view profile</a>
+            <g-link class="text-white capitalize text-sm font-light flex-grow text-center py-4" :to="{ path: `/users/${slug}`, params: { slug: 'slug' } }">View Profile</g-link>
         </div>
     </div>
 </template>
 
 <script>
     import VerifiedBadge from "./VerifiedBadge"
-
+    import { slugify } from '@/utils/generateSlug'
     export default {
         name: "UserCard",
         components: {
@@ -41,8 +41,11 @@
             }
         },
         computed: {
+            slug(){
+                return slugify(this.user.name)
+            },
             userProfile(){
-                return this.user.avatar ? this.user.avatar : `https://ui-avatars.com/api/?name=${this.user.name}&background=000000&color=fff&size=256&bold=true&rounded=true`
+                return this.user.avatar ? this.user.avatar : `https://ui-avatars.com/api/?name=${this.user.name}&background=000000&color=fff&size=256&bold=true`
             },
             userFirstName(){
                 return this.user.name.split(" ")[0]
@@ -86,47 +89,4 @@
     top: 35px;
 }
 
-//   <div class="card w-full md:w-1/3 lg:w-1/4 mt-4 mx-2 bg-white flex flex-col rounded-sm justify-between">
-//     <div class="flex w-full justify-between px-6 py-4">
-//         <g-image class="avatar w-1/4" alt="User Avatar" :src="userProfile" />
-//         <div class="user-attribute ml-2 w-3/4">
-//             <h5>{{ user.name }}</h5>
-//             <span>{{ user.location }} </span>
-//         </div>
-//     </div>
-//     <div class="text-lg flex my-2 px-6 flex-col">
-//         <h4>{{ user.currentRole }} </h4>
-//         <h4>{{ user.currentCompany }} </h4>
-//     </div>
-
-//     <div class="px-6 my-2 text-xs text-gray-600">
-//         <span>{{ user.bio }}</span>
-//     </div>
-
-//     <div class="flex w-full justify-end">
-//         <button class="text-xs bg-black right-0 bottom-0 hover:bg-white text-white hover:text-black font-bold py-2 px-4">
-//             <g-link :to="`/users/${user.id}`">View Profile</g-link>
-//         </button>
-//     </div>
-//   </div>
-// </template>
-
-
-// <style lang='scss'>
-//     .card {
-//         height: 300px;
-//     }
-//     .avatar {
-//         width: 56px;
-//         height: 56px;
-//     }
-//     .user-attribute{
-//         h5 {
-//             font-size: 14px;
-//             color: #828282;
-//         }
-//         span{
-//             font-size: 10px;
-//         }
-//     }
-// </style>
+</style>
